cbbf17643555fe0a4ee8de39af01d7c99bdb70b1
Gladturtle <akashsam88@gmail.com>
2024-11-20 19:09:01 +0530

    adding files

---
+++ test.py
@@ -0,0 +1,7 @@
+import requests,os
+#list_of_dir = os.listdir('D:\Programs\Python\Projects\API\static')
+#for dir in list_of_dir:
+  #  print('D:\\Programs\\Python\\Projects\\API\\static\\'+dir)
+   # request = requests.put('http://127.0.0.1:5000/image/1',json={'img':'D:\\Programs\\Python\\Projects\\API\\static\\'+dir})
+request = requests.post('http://127.0.0.1:5000/deleteuser')
+print(request.json())
